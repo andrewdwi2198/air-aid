ceff96c5cd2d5add7a499a14f6a0a1bf9ae9b1da
Ray Antonius <rayantonius@gmail.com>
2020-05-09 12:06:26 +0700

    add struct tags

--- helpers/models/models.go
+++ helpers/models/models.go
@@ -5,8 +5,8 @@ import "time"
 // Data holds the data being sent into the server
 type Data struct {
 	Timestamp time.Time `json:"time"`
-	Longitude float32   `json:"long"`
-	Latitude  float32   `json:"lat"`
-	CO        float32   `json:"co"`
-	NO2       float32   `json:"no2"`
+	Longitude float32   `json:"long" sql:"type:decimal(10,3);"`
+	Latitude  float32   `json:"lat" sql:"type:decimal(10,3);"`
+	CO        float32   `json:"co" sql:"type:decimal(10,3);"`
+	NO2       float32   `json:"no2" sql:"type:decimal(10,3);"`
 }
